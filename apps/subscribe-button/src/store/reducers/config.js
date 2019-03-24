5316fa576f861bb2577bb7c4a60d58b7a4425fdb
Letty <git@letty.io>
2019-03-24 21:41:49 +0100

    chore: add cover property to config

--- apps/subscribe-button/src/store/reducers/config.js
+++ apps/subscribe-button/src/store/reducers/config.js
@@ -6,13 +6,14 @@ export const INIT_STATE = {
   size: 'big',
   color: '#c90bd6',
   format: 'cover',
-  style: 'filled'
+  style: 'filled',
+  cover: 'https://meta.metaebene.me/media/podlove/podlove-logo-2.0-512x512.jpg'
 }
 
 export const reducer = handleActions({ [INIT]: (state, { payload }) => ({ ...state, ...payload }) }, INIT_STATE)
 
-// export const color = state => state.color;
 export const color = prop('color')
+export const cover = prop('cover')
 export const format = prop('format')
 export const size = prop('size')
 export const style = prop('style')
