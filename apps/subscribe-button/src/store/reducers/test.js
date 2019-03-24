487968d69993de8270551223354a7cac313dd9ec
Letty <git@letty.io>
2019-03-24 12:53:00 +0100

    Remove test reducer

--- apps/subscribe-button/src/store/reducers/test.js
+++
@@ -1,8 +0,0 @@
-import { handleActions } from 'redux-actions'
-
-export const INIT_STATE = {
-  title: null,
-  number: 0
-}
-
-export const reducer = handleActions({ foo: (state, action) => state }, INIT_STATE)
