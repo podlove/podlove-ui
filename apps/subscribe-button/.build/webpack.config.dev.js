f26d8b64112456fdcf61c57ffadcf2828d6477af
Letty <git@letty.io>
2019-03-24 11:36:00 +0100

    Add basic subscribe button folder

---
+++ apps/subscribe-button/.build/webpack.config.dev.js
@@ -0,0 +1,41 @@
+const { output, resolve, devServer, rules, plugins } = require('@podlove/build')
+
+module.exports = {
+  mode: 'development',
+
+  entry: {
+    main: './subscribe-button.js'
+    // example: './example/example.js'
+  },
+  output: output(),
+
+  resolve: resolve({
+    styles: './src/styles',
+    store: './src/store',
+    directives: './src/directives'
+  }),
+
+  devtool: 'inline-source-map',
+  devServer: devServer({ port: 9000, contentBase: './dist' }),
+
+  module: {
+    rules: [rules.vue(), rules.javascript(), rules.images(), rules.vueStyles({ prod: false }), rules.pug()]
+  },
+
+  plugins: [
+    plugins.vue(),
+    plugins.base('.'),
+    plugins.jarvis(1337),
+    plugins.bundleAnalyzer(),
+    plugins.hmr(),
+    ...plugins.html({
+      files: [
+        {
+          filename: 'index.html',
+          template: './example/index.html'
+        }
+      ]
+    }),
+    plugins.env({ mode: 'development' })
+  ]
+}
