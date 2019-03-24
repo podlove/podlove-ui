0398b26b52a91c8821c48d76b614a38e64467f34
Letty <git@letty.io>
2019-03-24 12:58:54 +0100

    Chore all selectors for config

--- apps/subscribe-button/src/store/selectors.js
+++ apps/subscribe-button/src/store/selectors.js
@@ -1,3 +1,6 @@
 import * as config from './reducers/config'
 
+export const selectColor = state => config.color(state.color)
+export const selectFormat = state => config.format(state.format)
+export const selectSize = state => config.size(state.size)
 export const selectStyle = state => config.style(state.config)
