259e4f3607412ad1f7fd714b7fe5cf4ee55dae0a
Letty <git@letty.io>
2019-03-24 21:45:14 +0100

    Chore: add button component Use computed class name from properties

--- apps/subscribe-button/src/App.vue
+++ apps/subscribe-button/src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <div :style="`background: ${color}`">
-    <div>cover</div>
-    <button>Subscribe</button>
-    <div>{{color}}</div>
-    {{style}}
+  <div :class="dimensions" :style="`background: ${color}`">
+    <cover v-if="cover" alt="ccc" :url="cover" :cover-color="color"/>
+    <button-component></button-component>
   </div>
 </template>
 
 <script>
+import ButtonComponent from "./button/button";
+import { Image } from "@podlove/components";
 import { mapState } from "redux-vuex";
 import {
   selectColor,
+  selectCover,
   selectFormat,
   selectSize,
   selectStyle
@@ -19,12 +20,27 @@ import {
 export default {
   data: mapState({
     color: selectColor,
+    cover: selectCover,
     format: selectFormat,
     size: selectSize,
     style: selectStyle
-  })
+  }),
+  computed: {
+    dimensions() {
+      return `${this.size.toLowerCase()} ${this.format.toLowerCase()} ${this.style.toLowerCase()}`;
+    }
+  },
+  components: { Cover: Image, ButtonComponent }
 };
 </script>
 
-<style>
+<style lang="scss">
+@import "~normalize-scss";
+@import "~theme/reset";
+@import "~theme/variable";
+
+.big {
+  width: $size-big-width;
+  height: $size-big-height;
+}
 </style>
