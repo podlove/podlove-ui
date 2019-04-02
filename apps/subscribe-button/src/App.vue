0caeff3910a2d6ecee042a433d0d720c5cb08daf
Letty <git@letty.io>
2019-04-02 18:22:42 +0200

    Chore styling of sb based on config

--- apps/subscribe-button/src/App.vue
+++ apps/subscribe-button/src/App.vue
@@ -27,7 +27,8 @@ export default {
   }),
   computed: {
     dimensions() {
-      return `${this.size.toLowerCase()} ${this.format.toLowerCase()} ${this.style.toLowerCase()}`;
+      return `${this.size.toLowerCase()}-${this.format.toLowerCase()}`;
+      // return `${this.size.toLowerCase()} ${this.format.toLowerCase()} ${this.style.toLowerCase()}`;
     }
   },
   components: { Cover: Image, ButtonComponent }
@@ -39,8 +40,69 @@ export default {
 @import "~theme/reset";
 @import "~theme/variable";
 
-.big {
+.big-rectangle {
+  width: $size-big-width;
+  height: $size-big-square-rectangle-height;
+
+  .image-container {
+    display: none;
+  }
+}
+
+.big-cover {
   width: $size-big-width;
   height: $size-big-height;
+}
+
+.big-square {
+  .image-container {
+    display: none;
+  }
+  width: $size-big-square-rectangle-height;
+  height: $size-big-square-rectangle-height;
+}
+
+.medium-rectangle {
+  width: $size-medium-width;
+  height: $size-medium-square-rectangle-height;
+
+  .image-container {
+    display: none;
+  }
+}
+
+.medium-cover {
+  width: $size-medium-width;
+  height: $size-medium-height;
+}
+
+.medium-square {
+  .image-container {
+    display: none;
+  }
+  width: $size-medium-square-rectangle-height;
+  height: $size-medium-square-rectangle-height;
+}
+
+.small-rectangle {
+  width: $size-small-width;
+  height: $size-small-square-rectangle-height;
+
+  .image-container {
+    display: none;
+  }
+}
+
+.small-cover {
+  width: $size-small-width;
+  height: $size-small-height;
+}
+
+.small-square {
+  .image-container {
+    display: none;
+  }
+  width: $size-small-square-rectangle-height;
+  height: $size-small-square-rectangle-height;
 }
 </style>
