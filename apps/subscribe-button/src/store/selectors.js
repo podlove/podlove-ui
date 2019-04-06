d48140c8067acb071f39566cbe2d7e24e4ecae81
Letty <login@letty.io>
2019-04-06 12:26:55 +0200

    Update selects for meta data

--- apps/subscribe-button/src/store/selectors.js
+++ apps/subscribe-button/src/store/selectors.js
@@ -1,7 +1,9 @@
 import * as config from './reducers/config'
+import * as metadata from './reducers/metadata'
 import { propOr, compose } from 'ramda'
 
 export const configSlice = propOr({}, 'config')
+export const metadataSlice = propOr({}, 'metadata')
 
 export const selectColor = compose(
   config.color,
@@ -22,4 +24,24 @@ export const selectSize = compose(
 export const selectStyle = compose(
   config.style,
   configSlice
+)
+
+export const selectTitle = compose(
+  metadata.title,
+  metadataSlice
+)
+
+export const selectSubTitle = compose(
+  metadata.subtitle,
+  metadataSlice
+)
+
+export const selectDescription = compose(
+  metadata.description,
+  metadataSlice
+)
+
+export const selectFeed = compose(
+  metadata.feed,
+  metadataSlice
 )
