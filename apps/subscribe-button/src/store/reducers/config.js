a636d743105c28c6c0cd24e0feda7e01165ced14
Letty <login@letty.io>
2019-04-06 12:23:10 +0200

    style

--- apps/subscribe-button/src/store/reducers/config.js
+++ apps/subscribe-button/src/store/reducers/config.js
@@ -10,7 +10,10 @@ export const INIT_STATE = {
   cover: 'https://meta.metaebene.me/media/podlove/podlove-logo-2.0-512x512.jpg'
 }
 
-export const reducer = handleActions({ [INIT]: (state, { payload }) => ({ ...state, ...payload }) }, INIT_STATE)
+export const reducer = handleActions(
+  { [INIT]: (state, { payload }) => ({ ...state, ...payload }) },
+  INIT_STATE
+)
 
 export const color = prop('color')
 export const cover = prop('cover')
