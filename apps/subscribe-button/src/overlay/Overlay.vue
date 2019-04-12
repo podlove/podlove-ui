b40507acd5c530e1b34c5299efd1e899b548515e
Letty <git@letty.io>
2019-04-12 21:00:18 +0200

    Chore: Overlay view

---
+++ apps/subscribe-button/src/overlay/Overlay.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <ul>
+      <li>{{ color }}</li>
+      <li>{{ cover }}</li>
+      <li>{{ podcastDescription }}</li>
+      <li>{{ podcastFeed }}</li>
+      <li>{{ podcastTitle }}</li>
+      <li>{{ podcastSubTitle }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'redux-vuex'
+import {
+  selectColor,
+  selectCover,
+  selectTitle,
+  selectSubTitle,
+  selectDescription,
+  selectFeed
+} from 'store/selectors'
+
+export default {
+  data: mapState({
+    color: selectColor,
+    cover: selectCover,
+    podcastDescription: selectDescription,
+    podcastFeed: selectFeed,
+    podcastTitle: selectTitle,
+    podcastSubTitle: selectSubTitle
+  })
+}
+</script>
