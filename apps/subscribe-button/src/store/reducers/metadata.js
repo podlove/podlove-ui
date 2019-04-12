6fa172baefb8e4b74dc6fe3f98e40489dde0dbb9
Letty <git@letty.io>
2019-04-12 20:54:17 +0200

    Add initial state with real data from freakshow podcast

--- apps/subscribe-button/src/store/reducers/metadata.js
+++ apps/subscribe-button/src/store/reducers/metadata.js
@@ -2,11 +2,29 @@ import { handleActions } from 'redux-actions'
 import { prop } from 'ramda'
 import { SET_METADATA } from './types'
 
+// export const INITIAL_STATE = {
+//   title: '',
+//   subtitle: '',
+//   description: '',
+//   feed: []
+// }
 export const INITIAL_STATE = {
-  title: '',
-  subtitle: '',
-  description: '',
-  feed: []
+  title: 'Freak Show',
+  subtitle: 'Menschen! Technik! Sensationen!',
+  description:
+    'Die muntere Talk Show um Leben mit Technik, das Netz und Technikkultur. Wir leben und lieben Technologie und reden dar\u00fcber.',
+  feed: [
+    {
+      type: 'audio',
+      format: 'aac',
+      url: 'https://freakshow.fm/feed/m4a',
+      variant: 'high',
+      'directory-url-itunes': 'https://itunes.apple.com/podcast/id277518737'
+    },
+    { type: 'audio', format: 'mp3', url: 'https://freakshow.fm/feed/mp3', variant: 'high' },
+    { type: 'audio', format: 'ogg', url: 'https://freakshow.fm/feed/oga', variant: 'high' },
+    { type: 'audio', format: 'opus', url: 'https://freakshow.fm/feed/opus', variant: 'high' }
+  ]
 }
 
 export const reducer = handleActions(
