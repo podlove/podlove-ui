bc0aa992f25827d1f7d0c713f8db275e59f459bb
Letty <git@letty.io>
2019-04-21 16:54:26 +0200

    Style: Reformatting

--- apps/subscribe-button/src/store/index.js
+++ apps/subscribe-button/src/store/index.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 
-import { compose, createStore } from 'redux'
+import { createStore } from 'redux'
 import { connect } from 'redux-vuex'
 
 import reducers from './reducers'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
-const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 connect({
   Vue,
