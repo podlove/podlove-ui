a3c062127b5533361bca56c83cc4153408862773
Letty <git@letty.io>
2019-04-26 16:49:11 +0200

    Chore: Introduce tabs as part of the state

--- apps/subscribe-button/src/store/reducers/index.js
+++ apps/subscribe-button/src/store/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux'
 import { reducer as config } from './config'
 import { reducer as metadata } from './metadata'
+import { reducer as tab } from './tab'
 
-export default combineReducers({ config, metadata })
+export default combineReducers({ config, metadata, tab })
