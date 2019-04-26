97ea56b7254e39ec0b40d7ee9b20e91cd98edc6f
Letty <git@letty.io>
2019-04-26 15:27:17 +0200

    Chore: Basic tab body component

---
+++ apps/subscribe-button/src/overlay/tabs/components/TabBody.vue
@@ -0,0 +1,31 @@
+<template>
+  <tab-body
+    v-if="active"
+    :ref="tab"
+    class="tab"
+    :active="active"
+    :name="tab"
+    :aria-selected="active"
+  >
+    <slot></slot>
+  </tab-body>
+</template>
+
+<script>
+import { Tab } from '@podlove/components'
+export default {
+  components: {
+    TabBody: Tab.Body
+  },
+  props: {
+    active: {
+      type: Boolean,
+      default: false
+    },
+    tab: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
