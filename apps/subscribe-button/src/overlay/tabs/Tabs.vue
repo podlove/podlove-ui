2839b9833fcd59f1568af78e9de8502e11ba0cc8
Letty <git@letty.io>
2019-04-26 21:55:48 +0200

    Refactor: Remove active from props in tabs

--- apps/subscribe-button/src/overlay/tabs/Tabs.vue
+++ apps/subscribe-button/src/overlay/tabs/Tabs.vue
@@ -1,28 +1,24 @@
 <template>
   <div class="tabs">
     <tab-header class="overflows">
-      <tab-header-item tab="apps" :title="$t('APPS.TITLE')" :active="false"></tab-header-item>
-      <tab-header-item tab="cloud" :title="$t('CLOUD.TITLE')" :active="false"></tab-header-item>
-      <tab-header-item
-        tab="platform"
-        :title="$t('PLATFORM.TITLE')"
-        :active="false"
-      ></tab-header-item>
-      <tab-header-item tab="info" :title="$t('INFO.TITLE')" :active="true"></tab-header-item>
+      <tab-header-item tab="apps" :title="$t('APPS.TITLE')"></tab-header-item>
+      <tab-header-item tab="cloud" :title="$t('CLOUD.TITLE')"></tab-header-item>
+      <tab-header-item tab="platform" :title="$t('PLATFORM.TITLE')"></tab-header-item>
+      <tab-header-item tab="info" :title="$t('INFO.TITLE')"></tab-header-item>
     </tab-header>
-    <tab-body tab="apps" :active="false">
+    <tab-body tab="apps">
       <div>something</div>
     </tab-body>
-    <tab-body tab="cloud" :active="false">
+    <tab-body tab="cloud">
       <div>something</div>
     </tab-body>
-    <tab-body tab="platform" :active="false">
+    <tab-body tab="platform">
       <div>
         something
         <h1>hello</h1>
       </div>
     </tab-body>
-    <tab-body tab="info" :active="true">
+    <tab-body tab="info">
       <info-tab></info-tab>
     </tab-body>
   </div>
