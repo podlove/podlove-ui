31413d4fdb27ef3f98cfae3f0e44410e402cab7b
Letty <git@letty.io>
2019-04-26 16:15:21 +0200

    Chore: Introduce info tab for information about subscribing to a podcast

---
+++ apps/subscribe-button/src/overlay/info/Info.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>{{ $t('INFO.HEADLINE') }}</h1>
+    <p v-for="(p, index) in $t('INFO.PARAGRAPHS')" :key="index">
+      {{ p }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
