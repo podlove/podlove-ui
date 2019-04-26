c5628615af7357dd3d39b6ca8d6deeb208d1f642
Letty <git@letty.io>
2019-04-26 11:21:12 +0200

    Chore: Introduce vue-i18n into the app

--- apps/subscribe-button/subscribe-button.js
+++ apps/subscribe-button/subscribe-button.js
@@ -1,11 +1,28 @@
 import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+
 import App from './src'
 import store from './src/store'
 
+Vue.use(VueI18n)
+
+// Use public path from application
+// __webpack_public_path__ = window.resourceBaseUrl || '/'
+
 window.PODLOVE_STORE = store
 
+const i18n = new VueI18n({
+  locale: 'de',
+  fallbackLocale: 'de',
+  messages: {
+    // en: require('./lang/en.json'),
+    de: require('./lang/de.json')
+    // eo: require('./lang/eo.json')
+  }
+})
+
 new Vue({
-  // i18n,
+  i18n,
   el: document.getElementsByTagName('subscribe-button')[0],
   render: h => h(App)
 })
