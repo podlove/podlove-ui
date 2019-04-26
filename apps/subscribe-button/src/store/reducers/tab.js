a8403102f1ac33cf52578c49f19195ed2b6ce83d
Letty <git@letty.io>
2019-04-26 16:49:11 +0200

    Chore: Introduce tabs as part of the state

---
+++ apps/subscribe-button/src/store/reducers/tab.js
@@ -0,0 +1,20 @@
+import { handleActions } from 'redux-actions'
+import { prop } from 'ramda'
+import { INIT } from './types'
+
+const INITIAL_STATE = {
+  apps: false,
+  cloud: false,
+  platform: false,
+  info: true
+}
+
+export const reducer = handleActions(
+  { [INIT]: (state, { payload }) => ({ ...state, ...payload }) },
+  INITIAL_STATE
+)
+
+export const apps = prop('apps')
+export const cloud = prop('cloud')
+export const platform = prop('platform')
+export const info = prop('info')
--- apps/subscribe-button/src/store/selectors.js
+++ apps/subscribe-button/src/store/selectors.js
@@ -1,9 +1,11 @@
 import * as config from './reducers/config'
 import * as metadata from './reducers/metadata'
+import * as tab from './reducers/tab'
 import { propOr, compose } from 'ramda'
 
 export const configSlice = propOr({}, 'config')
 export const metadataSlice = propOr({}, 'metadata')
+export const tabSlice = propOr({}, 'tab')
 
 export const selectColor = compose(
   config.color,
@@ -44,4 +46,24 @@ export const selectDescription = compose(
 export const selectFeed = compose(
   metadata.feed,
   metadataSlice
+)
+
+export const selectApps = compose(
+  tab.apps,
+  tabSlice
+)
+
+export const selectCloud = compose(
+  tab.cloud,
+  tabSlice
+)
+
+export const selectPlatform = compose(
+  tab.platform,
+  tabSlice
+)
+
+export const selectInfo = compose(
+  tab.info,
+  tabSlice
 )
