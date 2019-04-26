56289c6f9b6311fc9c2c331f6be81bdc74dc31fd
Letty <git@letty.io>
2019-04-26 16:16:17 +0200

    Chore: Use info tab in tab declaration

--- apps/subscribe-button/src/overlay/tabs/Tabs.vue
+++ apps/subscribe-button/src/overlay/tabs/Tabs.vue
@@ -6,9 +6,9 @@
       <tab-header-item
         tab="platform"
         :title="$t('PLATFORM.TITLE')"
-        :active="true"
+        :active="false"
       ></tab-header-item>
-      <tab-header-item tab="info" :title="$t('INFO.TITLE')" :active="false"></tab-header-item>
+      <tab-header-item tab="info" :title="$t('INFO.TITLE')" :active="true"></tab-header-item>
     </tab-header>
     <tab-body tab="apps" :active="false">
       <div>something</div>
@@ -16,14 +16,14 @@
     <tab-body tab="cloud" :active="false">
       <div>something</div>
     </tab-body>
-    <tab-body tab="platform" :active="true">
+    <tab-body tab="platform" :active="false">
       <div>
         something
         <h1>hello</h1>
       </div>
     </tab-body>
-    <tab-body tab="info" :active="false">
-      <div>something</div>
+    <tab-body tab="info" :active="true">
+      <info-tab></info-tab>
     </tab-body>
   </div>
 </template>
@@ -33,12 +33,23 @@ import { Tab } from '@podlove/components'
 
 import TabHeaderItem from './components/TabHeaderItem'
 import TabBody from './components/TabBody'
+import InfoTab from '../info/'
+
+// const tabs = {
+//   InfoTab: () =>
+//     import(/* webpackChunkName: "info-tab" */
+//     /* webpackMode: "lazy" */
+//     /* webpackPreload: true */
+//     '../info')
+// }
 
 export default {
   components: {
+    InfoTab,
     TabBody,
     TabHeader: Tab.Header,
     TabHeaderItem
+    // ...tabs
   }
 }
 </script>
