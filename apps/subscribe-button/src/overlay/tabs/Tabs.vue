d5863ef4c8a2b50968fcc902dbfb8e5b69f376bd
Letty <git@letty.io>
2019-04-26 10:40:50 +0200

    Chore: Introduce tabs for subscribe button, wip

---
+++ apps/subscribe-button/src/overlay/tabs/Tabs.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="tabs">
+    <tab-header class="overflows">
+      <tab-header-item title="tab 0" :active="true"></tab-header-item>
+      <tab-header-item title="tab 1" :active="false"></tab-header-item>
+      <tab-header-item title="tab 2" :active="false"></tab-header-item>
+      <tab-header-item title="tab 3" :active="false"></tab-header-item>
+    </tab-header>
+  </div>
+</template>
+
+<script>
+import { Tab } from '@podlove/components'
+
+import TabHeaderItem from './components/TabHeaderItem'
+export default {
+  components: {
+    TabHeader: Tab.Header,
+    TabHeaderItem
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.tabs {
+  width: 100%;
+
+  .overflows .tab-header-item >>> .title {
+    display: none;
+  }
+}
+</style>
---
+++ apps/subscribe-button/src/overlay/tabs/components/TabHeaderItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <tab-header-item :name="tab" :active="active">
+    <span>
+      {{ title }}
+    </span>
+  </tab-header-item>
+  <!-- tab-header-item(
+    :name="tab"
+    @click="toggleTab(tab)"
+    :color="header.color"
+    :colorActive="header.colorActive"
+    :background="header.background"
+    :backgroundActive="header.backgroundActive"
+    :active="active"
+  )
+    slot(name="icon" slot="icon")
+    span(slot="title") {{ title }} -->
+</template>
+
+<script>
+import { Tab } from '@podlove/components'
+export default {
+  components: {
+    TabHeaderItem: Tab.HeaderItem
+  },
+  props: {
+    title: {
+      type: String,
+      default: null
+    },
+    active: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
