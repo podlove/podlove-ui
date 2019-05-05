bfd6da37dbb8ab499584e81ebbdadfe11eb2545e
Letty <git@letty.io>
2019-05-05 12:50:37 +0200

    Refactor: Update platform detection to mac os and ios

--- packages/utils/useragent.js
+++ packages/utils/useragent.js
@@ -1,3 +1,12 @@
 export const getPlattform = () => {
-  return window.navigator.platform
+  let platf = window.navigator.platform.toLowerCase()
+  // console.log(platf)
+
+  if (platf.match(/mac/i) !== null) {
+    return 'osx'
+  }
+
+  if (platf.match(/(ipod|ipad|iphone)/i) !== null) {
+    return 'ios'
+  }
 }
