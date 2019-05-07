edfa351bfb07a5fa5a85192e462f439e279dab45
Letty <git@letty.io>
2019-05-07 11:24:14 +0200

    Refactor: Add android, unix and windoof detection

--- packages/utils/useragent.js
+++ packages/utils/useragent.js
@@ -9,4 +9,16 @@ export const getPlattform = () => {
   if (platf.match(/(ipod|ipad|iphone)/i) !== null) {
     return 'ios'
   }
+
+  if (platf.match(/android/i) !== null) {
+    return 'android'
+  }
+
+  if (platf.match(/(linux|openbsd|freebsd|netbsd)/i) !== null) {
+    return 'unix'
+  }
+
+  if (platf.match(/windows/i) !== null) {
+    return 'windows'
+  }
 }
