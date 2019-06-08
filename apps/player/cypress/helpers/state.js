ae24da45cf5e460c272a798be58149971f55f36a
Alexander Heimbuch <github@heimbu.ch>
2019-06-08 14:04:09 +0200

    fix(player-state): fix player state tests

--- apps/player/cypress/helpers/state.js
+++ apps/player/cypress/helpers/state.js
@@ -1,6 +1,6 @@
 const setState = (...fragments) => ({ PODLOVE_STORE }) => {
   const state = fragments.reduce((result, item) => Object.assign({}, result, item), {})
-  PODLOVE_STORE.dispatch({ type: 'INIT', payload: state })
+  PODLOVE_STORE.dispatch({ type: 'PLAYER_INIT', payload: state })
 }
 
 module.exports = { setState }
