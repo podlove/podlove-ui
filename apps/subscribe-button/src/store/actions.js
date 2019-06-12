04869b75a522300d601db3069f54737d9bc66125
Letty <login@letty.io>
2019-06-12 12:30:55 +0200

    Feat: Introducse hideOverlay action #67

--- apps/subscribe-button/src/store/actions.js
+++ apps/subscribe-button/src/store/actions.js
@@ -1,5 +1,6 @@
 import { createAction } from 'redux-actions'
-import { SHOW_OVERLAY, INIT } from './reducers/types'
+import { HIDE_OVERLAY, SHOW_OVERLAY, INIT } from './reducers/types'
 
+export const hideOverlay = createAction(HIDE_OVERLAY)
 export const showOverlay = createAction(SHOW_OVERLAY)
 export const init = createAction(INIT)
