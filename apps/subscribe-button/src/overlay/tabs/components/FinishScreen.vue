9868d55c4ef878d5d081255aac0d051ddc8f7b52
Letty <login@letty.io>
2019-06-18 09:49:31 +0200

    Refactor: Remove props and use store property for client #68

--- apps/subscribe-button/src/overlay/tabs/components/FinishScreen.vue
+++ apps/subscribe-button/src/overlay/tabs/components/FinishScreen.vue
@@ -1,31 +1,26 @@
 <template>
   <div>
     <div>logoplatzhalter</div>
-    <h1>{{ $t('FINISH-SCREEN.TITLE', { client: 'iTunes' }) }}</h1>
+    <h1>{{ $t('FINISH-SCREEN.TITLE', { client: finishObject.title }) }}</h1>
     <div>{{ $t('FINISH-SCREEN.SOMETHING-WENT-WRONG') }}</div>
     <button>{{ $t('FINISH-SCREEN.TRY-AGAIN') }}</button>
     <p>
       <a href="">
-        {{ $t('FINISH-SCREEN.INSTALL', { client: 'iTunes' }) }}
+        {{ $t('FINISH-SCREEN.INSTALL', { client: finishObject.title }) }}
       </a>
     </p>
   </div>
 </template>
 
 <script>
+import { selectFinishScreenObject } from 'store/selectors'
+
 export default {
-  props: {
-    client: {
-      type: String,
-      default: null
-    },
-    url: {
-      type: String,
-      default: null
-    },
-    install: {
-      type: String,
-      default: null
+  data() {
+    return {
+      ...this.mapState({
+        finishObject: selectFinishScreenObject
+      })
     }
   }
 }
