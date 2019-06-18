201183dadf3451a8d412119083fca2d0a9bfb0d9
Letty <login@letty.io>
2019-06-18 09:43:20 +0200

    Feat: Introduce reducers and actions for client info in finish screen #68

--- apps/subscribe-button/src/store/actions.js
+++ apps/subscribe-button/src/store/actions.js
@@ -4,6 +4,8 @@ import {
   SHOW_OVERLAY,
   SHOW_FINISH_SCREEN,
   CLOSE_FINISH_SCREEN,
+  FILL_FINISH_OBJECT,
+  RESET_FINISH_OBJECT,
   INIT
 } from './reducers/types'
 
@@ -11,4 +13,6 @@ export const hideOverlay = createAction(HIDE_OVERLAY)
 export const showOverlay = createAction(SHOW_OVERLAY)
 export const closeFinishScreen = createAction(CLOSE_FINISH_SCREEN)
 export const showFinishScreen = createAction(SHOW_FINISH_SCREEN)
+export const fillFinishObject = createAction(FILL_FINISH_OBJECT)
+export const resetFinishObject = createAction(RESET_FINISH_OBJECT)
 export const init = createAction(INIT)
--- apps/subscribe-button/src/store/reducers/view.js
+++ apps/subscribe-button/src/store/reducers/view.js
@@ -1,7 +1,14 @@
 import { prop } from 'ramda'
 import { handleActions } from 'redux-actions'
 
-import { SHOW_OVERLAY, HIDE_OVERLAY, SHOW_FINISH_SCREEN, CLOSE_FINISH_SCREEN } from './types'
+import {
+  SHOW_OVERLAY,
+  HIDE_OVERLAY,
+  SHOW_FINISH_SCREEN,
+  CLOSE_FINISH_SCREEN,
+  FILL_FINISH_OBJECT,
+  RESET_FINISH_OBJECT
+} from './types'
 
 export const INITIAL_STATE = {
   overlay: false,
@@ -13,8 +20,10 @@ export const reducer = handleActions(
   {
     [SHOW_OVERLAY]: state => ({ ...state, overlay: true }),
     [HIDE_OVERLAY]: state => ({ ...state, overlay: false, finish: false, finish_object: null }),
-    [SHOW_FINISH_SCREEN]: (state, { payload }) => ({ ...state, finish: true, ...payload }),
-    [CLOSE_FINISH_SCREEN]: state => ({ ...state, finish: false, finish_object: null })
+    [SHOW_FINISH_SCREEN]: state => ({ ...state, finish: true }),
+    [CLOSE_FINISH_SCREEN]: state => ({ ...state, finish: false }),
+    [FILL_FINISH_OBJECT]: (state, { payload }) => ({ ...state, ...payload }),
+    [RESET_FINISH_OBJECT]: state => ({ ...state, finish_object: null })
   },
   INITIAL_STATE
 )
