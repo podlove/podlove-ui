d6c1e2ec71bab46794418f2601751a892d25615f
Letty <login@letty.io>
2019-07-28 15:15:04 +0200

    Refactor: remove styling

--- apps/subscribe-button/src/embed/overlay.js
+++ apps/subscribe-button/src/embed/overlay.js
@@ -6,11 +6,7 @@ const overlayStyles = {
   width: '100%',
   height: '100%',
   top: 0,
-  left: 0,
-  background: 'rgba(0, 0, 0, 0.6)',
-  display: 'flex',
-  'justify-content': 'center',
-  'align-items': 'center'
+  left: 0
 }
 
 export default (store, entry) => {
