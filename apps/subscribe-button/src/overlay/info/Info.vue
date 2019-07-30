bad5be681d7e44c84e58cb4be5d13c3356e21d89
Letty <login@letty.io>
2019-07-30 11:32:46 +0200

    Refactor: Style information overlay #71

--- apps/subscribe-button/src/overlay/info/Info.vue
+++ apps/subscribe-button/src/overlay/info/Info.vue
@@ -1,22 +1,37 @@
 <template>
-  <div>
+  <div class="info-overlay" :style="backgroundGradient">
     <button id="close-button" @click="hideInfo">
       <icon type="close" :size="10"></icon>
     </button>
-    <h1>{{ $t('INFO.HEADLINE') }}</h1>
-    <p v-for="(p, index) in $t('INFO.PARAGRAPHS')" :key="index">
-      {{ p }}
-    </p>
+    <div class="info-content">
+      <h1>{{ $t('INFO.HEADLINE') }}</h1>
+      <p v-for="(p, index) in $t('INFO.PARAGRAPHS')" :key="index">
+        {{ p }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
+import { selectColor } from 'store/selectors'
 import { hideInfo } from 'store/actions'
 import store from 'store'
 
 import { Icon } from '@podlove/components'
 export default {
   components: { Icon },
+  data() {
+    return {
+      ...this.mapState({
+        color: selectColor
+      })
+    }
+  },
+  computed: {
+    backgroundGradient() {
+      return 'background-image: linear-gradient(rgba(184, 239, 250, 1.0) 90%, rgba(184, 239, 250, 0.7) 100%);'
+    }
+  },
   methods: {
     hideInfo() {
       store.dispatch(hideInfo())
@@ -26,9 +41,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.info-overlay {
+  z-index: 99;
+  -moz-border-radius-topleft: 10px;
+  -moz-border-radius-topright: 10px;
+  -webkit-border-top-left-radius: 10px;
+  -webkit-border-top-right-radius: 10px;
+}
+.info-content {
+  padding: 0 1.75em;
+  h1 {
+    margin-top: 0;
+  }
+}
+
 #close-button {
   width: auto;
-  margin: 2px;
+  margin: 0.5em;
   padding: 4px;
   border-radius: 50%;
   border: 1px solid black;
