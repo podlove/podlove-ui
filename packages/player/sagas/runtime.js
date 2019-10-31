3fc6fd945294dc47c15f771c1635598de914ac0c
Alexander Heimbuch <github@heimbu.ch>
2019-10-31 13:15:18 +0100

    chore(player-saga): fix browser-detect call

--- packages/player/sagas/runtime.js
+++ packages/player/sagas/runtime.js
@@ -1,12 +1,12 @@
 import { put } from 'redux-saga/effects'
 import { setRuntime } from '@podlove/player-actions/runtime'
 import { head } from 'ramda'
-import browser from 'detect-browser'
+import { detect } from 'detect-browser'
 import { isMobile } from '@podlove/utils/detect'
 
-const locale = navigator.language || navigator.userLanguage || 'en-us'
-
 export function* runtimeSaga() {
+  const browser = detect()
+  const locale = navigator.language || navigator.userLanguage || 'en-us'
   yield put(
     setRuntime({
       browser: `${browser.name}:${browser.version}`,
