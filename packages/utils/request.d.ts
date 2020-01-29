4ea70542fa0c3e4bbe441391033f40e61af608ca
Andreas Houben <andreas.houben@googlemail.com>
2020-01-29 16:16:27 +0100

    add typescript for json to request.d.ts

--- packages/utils/request.d.ts
+++ packages/utils/request.d.ts
@@ -1,8 +1,15 @@
-    /**
-     * Uses fetch to request the given URL
-     * @returns{(null|string)} The text field of the response or null if status != 200 or content type not 'text/html'
-     * 
-     * @param templateUrl The url to get
-     */
-    export function html(templateUrl: string):Promise<string>;
+/**
+ * Uses fetch to request html from the given URL
+ * @returns{Promise<(null|string)>} The text field of the response or null if status != 200 or content type not 'text/html'
+ * 
+ * @param templateUrl The url to fetch
+ */
+export function html(templateUrl: string): Promise<string|null>;
 
+
+/**
+ * Uses fetch to request JSON from the given URL * 
+ * @returns{Promise<T>} A promise of the json object typed by T
+ * @param url The url to fetch
+ */
+export function json<T>(url: string): Promise<T>;
