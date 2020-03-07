6bb6d1880a14f926e725d83620153c1b7e41c057
Alexander Heimbuch <github@heimbu.ch>
2020-03-07 12:08:02 +0100

    chore(clients): use services to redirect to castro, apple-podcasts and google-podcasts apps

--- packages/clients/src/castro/index.js
+++ packages/clients/src/castro/index.js
@@ -4,9 +4,9 @@ import icon from './icon.svg'
 export default [
   client({
     title: 'Castro',
-    scheme: feed => `castro://subscribe/${feed}`,
+    scheme: (itunesId = '') => `https://castro.fm/itunes/${itunesId.replace('id', '')}`,
     install: 'https://itunes.apple.com/app/castro-2/id1080840241',
-    type: type.app,
+    type: type.service,
     platform: platform.ios,
     icon
   })
