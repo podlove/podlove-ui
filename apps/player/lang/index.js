b2d2aa8556b6b9a5fb5642201d99ec398cddafc6
Alexander Heimbuch <github@heimbu.ch>
2022-10-08 09:59:27 +0200

    chore(web-player): register czech translation

--- apps/player/lang/index.js
+++ apps/player/lang/index.js
@@ -2,5 +2,6 @@ export default {
   en: require('./en.json'),
   de: require('./de.json'),
   eo: require('./eo.json'),
-  fr: require('./fr.json')
+  fr: require('./fr.json'),
+  cs: require('./cs.json')
 }
