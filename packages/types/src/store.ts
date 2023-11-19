f6e0d61f4fa7a1bb2e5d321d821df21b95d00d1a
Alexander Heimbuch <github@heimbu.ch>
2023-11-19 10:44:28 +0100

    dev bump

--- packages/types/src/store.ts
+++ packages/types/src/store.ts
@@ -1,4 +1,4 @@
-import type { Store } from '@reduxjs/toolkit';
-import type { PodloveWebPlayerStoreAction } from './store-action';
+import { Store } from 'redux';
+import { PodloveWebPlayerStoreAction } from './store-action.js';
 
 export type PodloveWebPlayerStore = Store<PodloveWebPlayerStoreAction>;
