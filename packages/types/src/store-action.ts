3f1c63390fc2718fa61826c8780c2fff52ecd829
Alexander Heimbuch <github@heimbu.ch>
2023-12-01 07:35:52 +0100

    fix: release

--- packages/types/src/store-action.ts
+++
@@ -1,8 +0,0 @@
-import type { PodloveWebPlayerStoreActionType } from "./store-action-type";
-
-export interface PodloveWebPlayerStoreAction {
-  type?: PodloveWebPlayerStoreActionType;
-  lastAction?: PodloveWebPlayerStoreAction;
-  payload?: number;
-  "@@redux-saga/SAGA_ACTION"?: boolean;
-}
