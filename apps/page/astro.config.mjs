32cd1c14e58697710832a9827a4013991b11dacc
Alexander Heimbuch <github@heimbu.ch>
2024-07-14 18:54:45 +0200

    feat(page): add loading overlay

--- apps/page/astro.config.mjs
+++ apps/page/astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 import node from '@astrojs/node';
 import vue from '@astrojs/vue';
 
@@ -7,11 +8,13 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
+  // adapter: node({
+  //   mode: 'standalone'
+  // }),
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
   }),
-  integrations: [
-    vue({ appEntrypoint: '/src/app' }),
-    tailwind()
-  ]
+  integrations: [vue({ appEntrypoint: '/src/app' }), tailwind()]
 });
--- apps/page/src/features/LoadingOverlay.vue
+++ apps/page/src/features/LoadingOverlay.vue
@@ -1,15 +1,37 @@
 <template>
-  <custom-transition type="fade">
-    <div class="fixed top-0 left-0 w-screen h-screen z-50 bg-gray-100" v-if="!state.initialized"></div>
-  </custom-transition>
+  <div
+    class="fixed top-0 left-0 w-screen h-screen z-50 bg-gray-100 loading-overlay"
+    :class="{ done, hidden }"
+  ></div>
 </template>
 
 <script setup lang="ts">
+import { computed, ref, watch } from 'vue';
 import { mapState } from 'redux-vuex';
 import { selectors } from '../logic/store';
-import CustomTransition from '../components/CustomTransition.vue';
+
+const hidden = ref(false);
 
 const state = mapState<{ initialized: boolean }>({
   initialized: selectors.colors.initialized
 });
+
+const done = computed(() => state.initialized);
+
+watch(done, () => {
+  setTimeout(() => {
+    hidden.value = true;
+  }, 250);
+});
 </script>
+
+<style scoped>
+.loading-overlay {
+  opacity: 1;
+  transition: opacity 250ms;
+}
+
+.loading-overlay.done {
+  opacity: 0;
+}
+</style>
