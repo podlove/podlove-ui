89f5fc1bfb9b5b8c9b8cd40f62834343c66c52cc
Alexander Heimbuch <github@heimbu.ch>
2024-11-16 09:07:00 +0100

    feat(translations): add arabic translations

---
+++ apps/subscribe-button/lang/ar.ts
@@ -0,0 +1,22 @@
+export default {
+  SUBSCRIBE: 'متابعة',
+  BACK_TO_LIST: 'العودة إلى الشاشة الرئيسية',
+  FINISH_SCREEN: {
+    REDIRECT_MESSAGE: "يتم إيصالك الآن. في حال لم تتم إعادة توجيهك، ",
+    REDIRECT_LINK: 'يرجى الضغط هنا.',
+    DOWNLOAD_APP: 'أو تنزيل التطبيق:',
+    COPY_MESSAGE: 'يرجى نسخ الرابط التالي وإضافته في تطبيق للبودكاست أو تطبيق لخلاصة RSS من اختيارك.',
+    COPY_URL: 'نسخ الرابط'
+  },
+  MESSAGES: {
+    COPIED: 'تم النسخ!'
+  },
+  A11Y: {
+    CLIENT: 'المتابعة عبر {title}',
+    CLOSE: 'إغلاق',
+    BACK: 'العودة إلى قائمة المنصات',
+    DOWNLOAD_APP: 'تنزيل التطبيق',
+    COPY_FEED: 'نسخ  رابط الـRSS',
+    FEED: 'خلاصة الRSS للبودكاست'
+  }
+}
