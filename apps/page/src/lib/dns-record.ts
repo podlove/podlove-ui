8ba382e2ae32dfd05b1f468bf6ae79903fd53d6c
Alexander Heimbuch <github@heimbu.ch>
2025-01-25 08:55:18 +0100

    chore(page): remove KV

--- apps/page/src/lib/dns-record.ts
+++ apps/page/src/lib/dns-record.ts
@@ -1,4 +1,4 @@
-import { get, noop } from 'lodash-es';
+import { get } from 'lodash-es';
 import type { APIContext } from 'astro';
 import { getDomain } from 'tldts';
 import { safeParse } from './json';
@@ -20,44 +20,31 @@ const getDnsRecords = async (hostname: string): Promise<DnsAnswer[]> =>
     .then((res) => res.json())
     .then((result) => get(result, ['Answer'], []));
 
-const getStore = (context: APIContext): KVNamespace =>
-  get(context, ['locals', 'runtime', 'env', 'CUSTOM_DOMAINS'], {
-    get: async () => null,
-    put: noop
-  } as unknown as KVNamespace);
-
 export const extractDnsData = async (context: APIContext): Promise<FeedData> => {
   const domain = getDomain(context.url.hostname);
   const entryName = `lux.${domain}`;
-  const store = getStore(context);
   const fallback = { feed: null, primary_color: null };
 
-  let result = await store.get(entryName);
+  const result = await getDnsRecords(entryName)
+    .then(
+      ([result]) => get(result, 'data', null),
+      () => null
+    )
+    .then((result) => {
+      if (!result) {
+        return null;
+      }
+
+      try {
+        return atob(result.replace(/['"]+/g, ''));
+      } catch (err) {
+        return null;
+      }
+    });
 
   if (!result) {
-    result = await getDnsRecords(entryName)
-      .then(
-        ([result]) => get(result, 'data', null),
-        () => null
-      )
-      .then((result) => {
-        if (!result) {
-          return null;
-        }
-
-        try {
-          return atob(result.replace(/['"]+/g, ''));
-        } catch (err) {
-          return null;
-        }
-      });
+    return fallback;
   }
-
-  if (!result) {
-    result = JSON.stringify(fallback);
-  }
-
-  store.put(entryName, result, { expirationTtl: 60 * 60 });
 
   return safeParse<FeedData>(result, fallback);
 };
